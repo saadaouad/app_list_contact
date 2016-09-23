8daa8e03f0236a6df4326f91103b1bd2209790bd
saadaouad <aouadsaad05@gmail.com>
2016-09-23 02:37:49 +0100

    add the method get & post

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,11 +1,22 @@
 angular.module('myApp', [])
 .controller('myController', function($scope, $http){
 
+var refresh = function(){
 	$http.get("/contactList").success(function(response){
-
 		console.log("I got the data I requested");
-
 		$scope.contactList = response;
+		$scope.contact = "";
 
 	});
+};
+
+refresh();
+
+	$scope.addContact = function(){
+		console.log($scope.contact);
+		$http.post('/contactList', $scope.contact). success(function(response){
+			console.log(response);
+			refresh();
+		});
+	};
 })
